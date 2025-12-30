fe54ca581f3501add7b20962722beef409be643e
Pranav Menon <pranavmenon@Pranavs-MacBook-Pro.local>
2025-12-29 20:09:54 -0800

    Remove unused import from login form

--- app/login/login-form.tsx
+++ app/login/login-form.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import { createClient } from '@/lib/supabase/client'
 import { FlowerLogo } from '@/components/flower-logo'
 
